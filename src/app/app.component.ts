63e5fe9c62cb6dede234cbffe0e0783985c6483a
W0852328 <w0852328@myscc.ca>
2024-09-20 12:12:16 -0400

    Assignment 2 Changed version to 0.1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { User } from './shared/models/user';
+import {CommonModule} from "@angular/common";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+
 })
+
+
 export class AppComponent {
-  title = 'JamieVirtueLearningAngular';
-  name : string = 'Jamie Virtue';
-  program : string = 'Computer Programming';
+  title= 'User Generation';
+  user1 : User = {id: 1, firstName: "Jamie", lastName: "Virtue",
+    department: "Programming", isAdmin: false};
+  user2 : User = {id: 2, firstName: "Darren", lastName:
+      "Takaki", department: "Web Dev", isAdmin: true};
+  //Can declare values either way
+  userList: User[] = //any[] would have worked as well
+    [this.user1,this.user2,
+      {id: 3, firstName: "John", lastName: "Doe", department:
+          "Programming", isAdmin: false},
+      {id: 4, firstName: "Jane", lastName: "Doe", department:
+          "Programming", isAdmin:true}
+    ]
 }
