93bc522bbb87c558e6c7dc9c1d4ed89c2ffa7b4d
W0852328 <w0852328@myscc.ca>
2024-09-27 10:21:04 -0400

    Assignment 3 Created * package.json changed to version 0.2.0 * shoeList and shoeListItem components created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { User } from './shared/models/user';
 import { Shoe } from './shared/models/user';
 import {CommonModule} from "@angular/common";
 import {NgIf} from "@angular/common";
