94673e766784eaea055b6aa772873df6595e132a
W0852328 <w0852328@myscc.ca>
2024-09-27 11:37:47 -0400

    Assignment 3 * Bonus with unique images per shoe

--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -1,10 +1,10 @@
-export interface User{
-  id: number;
-  firstName: string;
-  lastName: string;
-  department: string;
-  isAdmin?: boolean;
-}
+// export interface User{
+//   id: number;
+//   firstName: string;
+//   lastName: string;
+//   department: string;
+//   isAdmin?: boolean;
+// }
 
 export interface Shoe{
   colour: string;
--- src/app/shoe-list/shoe-list.component.ts
+++ src/app/shoe-list/shoe-list.component.ts
@@ -17,7 +17,7 @@ export class ShoeListComponent {
   shoe1 : Shoe = {colour: "Red", size: 10, brand: "Jordan", price: 250, isUsed: false};
   shoe2 : Shoe = {colour: "Gray", size: 11, brand: "Nike", price: 170, isUsed: false};
   shoe3 : Shoe = {colour: "Black & White", size: 13, brand: "Adidas", price: 200, isUsed: true};
-  shoe4 : Shoe = {colour: "Red", size: 7, brand: "Jordan", price: 120, isUsed: false};
+  shoe4 : Shoe = {colour: "Red", size: 7, brand: "Jordan 2", price: 120, isUsed: false};
   shoe5 : Shoe = {colour: "Green", size: 9, brand: "Sketchers", price: 60, isUsed: true};
   shoe6 : Shoe = {colour: "White & Blue", size: 10, brand: "Puma", price: 100, isUsed: false};
 
