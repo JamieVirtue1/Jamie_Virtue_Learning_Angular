fb663119a0baf220ffb60feb5c66dad9a876a19f
W0852328 <w0852328@myscc.ca>
2024-10-01 10:38:59 -0400

    Assignment 4 * Moved ShoeContentList to data/mock-content.ts * Created service named Shoes

--- src/app/shoe-list/shoe-list.component.ts
+++ src/app/shoe-list/shoe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Shoe} from "../shared/models/user";
+/**import {Shoe} from "../shared/models/user";*/
 import {ShoeListItemComponent} from "../shoe-list-item/shoe-list-item.component";
 import {NgForOf} from "@angular/common";
 
@@ -13,15 +13,9 @@ import {NgForOf} from "@angular/common";
   templateUrl: './shoe-list.component.html',
   styleUrl: './shoe-list.component.css'
 })
-export class ShoeListComponent {
-  shoe1 : Shoe = {colour: "Red", size: 10, brand: "Jordan", price: 250, isUsed: false};
-  shoe2 : Shoe = {colour: "Gray", size: 11, brand: "Nike", price: 170, isUsed: false};
-  shoe3 : Shoe = {colour: "Black & White", size: 13, brand: "Adidas", price: 200, isUsed: true};
-  shoe4 : Shoe = {colour: "Red", size: 7, brand: "Jordan 2", price: 120, isUsed: false};
-  shoe5 : Shoe = {colour: "Green", size: 9, brand: "Sketchers", price: 60, isUsed: true};
-  shoe6 : Shoe = {colour: "White & Blue", size: 10, brand: "Puma", price: 100, isUsed: false};
 
-  shoeList: Shoe[] = [this.shoe1, this.shoe2, this.shoe3, this.shoe4, this.shoe5,this.shoe6]
+export class ShoeListComponent {
+
 }
 
 
