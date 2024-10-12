21023529e1418d30b951cab2ba619930469b4b5b
Jamie Virtue <w0852328@myscc.ca>
2024-10-12 13:26:03 -0400

    Assignment 5 * Edited main.ts for routes * Imported provideRouter * Added routes variable with different routes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter} from "@angular/router";
+import {ShoeListComponent} from "./app/shoe-list/shoe-list.component";
+import {ShoeListItemComponent} from "./app/shoe-list-item/shoe-list-item.component";
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+const routes = [
+  {path: '', component:ShoeListComponent },
+  {path: 'shoe/:id', component: ShoeListItemComponent}
+]
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+});
+
+
